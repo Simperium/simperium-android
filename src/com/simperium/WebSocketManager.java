b33b22583098c0da3cac6f08d50bd27c60492745
Beau Collins <beaucollins@gmail.com>
2013-02-07 22:54:58 -0800

    Heartbeat implementation

--- src/com/simperium/WebSocketManager.java
+++ src/com/simperium/WebSocketManager.java
@@ -16,19 +16,27 @@ import java.util.Set;
 import java.util.HashMap;
 import org.apache.http.message.BasicNameValuePair;
 
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class WebSocketManager implements WebSocketClient.Listener, Channel.Listener {
     
     private static final String WEBSOCKET_URL = "wss://api.simperium.com/sock/websocket";
     private static final String SOCKETIO_URL = "https://api.simperium.com/";
     private static final String USER_AGENT_HEADER = "User-Agent";
-    
+    private static final String COMMAND_HEARTBEAT = "h";
     private String appId;
     private String clientId;
     private WebSocketClient socketClient;
     private boolean connected = false;
     private HashMap<Channel,Integer> channels;
     private HashMap<Integer,Channel> channelIndex;
+    
+    static final long HEARTBEAT_INTERVAL = 20000; // 20 seconds
+    
+    private Timer heartbeatTimer;
+    private int heartbeatCount = 0;
+    
     
     public WebSocketManager(String appId){
         this.appId = appId;
@@ -46,15 +54,14 @@ public class WebSocketManager implements WebSocketClient.Listener, Channel.Liste
      */
     public Channel createChannel(Bucket bucket, User user){
         // create a channel
-        Channel channel = new Channel(bucket, user);
-        channel.addListener(this);
+        Channel channel = new Channel(bucket, user, this);
         int channelId = channels.size();
         channels.put(channel, channelId);
         channelIndex.put(channelId, channel);
         // if we're connected tell the channel to fire off it's init message
         // otherwise we need to wait until we're connected
         if (isConnected()) {
-            channel.initialize(appId);
+            channel.start(appId);
         } else {
             connect();
         }
@@ -82,9 +89,25 @@ public class WebSocketManager implements WebSocketClient.Listener, Channel.Liste
         Iterator<Channel> iterator = channelSet.iterator();
         while(iterator.hasNext()){
             Channel channel = iterator.next();
-            channel.initialize(appId);
+            channel.start(appId);
         }
     }
+    
+    private void scheduleHeartbeat(){
+        if(heartbeatTimer != null) heartbeatTimer.cancel();
+        heartbeatTimer = new Timer();
+        heartbeatTimer.schedule(new TimerTask(){
+            public void run(){
+                sendHearbeat();
+            }
+        }, HEARTBEAT_INTERVAL);
+    }
+    
+    private void sendHearbeat(){
+        heartbeatCount ++;
+        socketClient.send(String.format("%s:%d", COMMAND_HEARTBEAT, heartbeatCount));
+    }
+    
     /**
      *
      * Channel.Listener event listener
@@ -108,7 +131,13 @@ public class WebSocketManager implements WebSocketClient.Listener, Channel.Liste
         initializeChannels();
     }
     public void onMessage(String message){
-        String[] parts = message.split(":", 2);
+        scheduleHeartbeat();
+        Simperium.log(String.format("Inbound: %s", message));
+        String[] parts = message.split(":", 2);;
+        if (parts[0].equals(COMMAND_HEARTBEAT)) {
+            heartbeatCount = Integer.parseInt(parts[1]);
+            return;
+        }
         int channelId = Integer.parseInt(parts[0]);
         Channel channel = channelIndex.get(channelId);
         channel.receiveMessage(parts[1]);
@@ -121,8 +150,7 @@ public class WebSocketManager implements WebSocketClient.Listener, Channel.Liste
         setConnected(false);
     }
     public void onError(Exception error){
-        org.apache.http.client.HttpResponseException httpError = (org.apache.http.client.HttpResponseException)error;
-        Simperium.log(String.format("Error: %d %s", httpError.getStatusCode(), httpError.getMessage()));
+        Simperium.log(String.format("Error: %s", error));
         setConnected(false);
     }
     
