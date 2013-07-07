2e2a8db766927e3607df032ad260ea2396169c5c
Robert Collins <robert@automattic.com>
2013-07-07 08:11:24 -0700

    Refactoring test app and adding ListActivity

---
+++ tests/app/src/com/simperium/tests/AppActivity.java
@@ -0,0 +1,60 @@
+package com.simperium.tests;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Properties;
+
+import java.io.InputStream;
+
+public abstract class AppActivity extends Activity
+{
+    public static String TAG="SimperiumTest";
+    private Properties mProperties;
+    /** Called when the activity is first created. */
+
+    public String getSimperiumAppId(){
+        return getProperty("simperium.appid");
+    }
+
+    public String getSimperiumAppSecret(){
+        return getProperty("simperium.appsecret");
+    }
+
+    public String getSimperiumUserToken(){
+        return getProperty("simperium.user.token");
+    }
+
+    public String getSimperiumUserEmail(){
+        return getProperty("simperium.user.email");
+    }
+
+    private void loadProperties() throws java.io.IOException {
+        if (mProperties == null) {
+            InputStream stream = getResources().getAssets().open("simperium.properties");
+            mProperties = new Properties();
+            mProperties.load(stream);
+        }
+    }
+
+    public String getProperty(String property){
+        return getProperty(property, "");
+    }
+
+    public String getProperty(String property, String fallback){
+        return getProperties().getProperty(property, fallback);
+    }
+
+    public Properties getProperties(){
+        if (mProperties == null) {
+            try {
+                loadProperties();                
+            } catch (java.io.IOException e) {
+                Log.e(TAG, "Unable to load simperium.properties", e);
+                mProperties = new Properties();
+            }
+        }
+        return mProperties;
+    }
+}
---
+++ tests/app/src/com/simperium/tests/ListActivity.java
@@ -0,0 +1,14 @@
+/**
+ * For testing the ObjectCursorLoader
+ */
+package com.simperium.tests;
+
+import android.os.Bundle;
+
+public class ListActivity extends android.app.ListActivity
+{
+    public static String TAG="SimperiumTest";
+
+    private static final int OBJECT_LOADER=0x0;
+
+}
