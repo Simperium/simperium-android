dc55d4896e0a75bb0cb0489574f3217b639c70be
Robert Collins <robert@automattic.com>
2013-07-10 11:41:41 -0700

    Fixing integration tests to work with updated Simperium API

--- tests/integration_tests/src/com/simperium/tests/models/Farm.java
+++ tests/integration_tests/src/com/simperium/tests/models/Farm.java
@@ -9,23 +9,32 @@ import java.util.Map;
 public class Farm extends BucketObject {
     public static String BUCKET_NAME="farm";
 
-    public static class Schema implements BucketSchema<Farm> {
+    public static class Schema extends BucketSchema<Farm> {
         private String suffix = "";
         private String remoteName = Farm.BUCKET_NAME;
+
         public Schema(){
             super();
         }
+
         public Schema(String suffix){
             remoteName = String.format("%s%s", remoteName, suffix);
         }
+
         @Override
         public String getRemoteName(){
             return remoteName;
         }
+
         @Override
         public Farm build(String key, Map<String,Object> properties){
             Farm farm = new Farm(key, properties);
             return farm;
+        }
+
+        @Override
+        public void update(Farm farm, Map<String,Object> properties){
+            farm.setProperties(properties);
         }
     }
 
@@ -35,6 +44,10 @@ public class Farm extends BucketObject {
 
     public Map<String,Object> getProperties(){
         return properties;
+    }
+
+    protected void setProperties(Map<String,Object> properties){
+        this.properties = properties;
     }
 
     public void setLogCount(int i){
