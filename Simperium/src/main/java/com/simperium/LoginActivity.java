7061dd34240197b7fc6d096c90ee32c66af7bef4
Dan Roundhill <dan@automattic.com>
2013-08-14 13:40:09 -0700

    Login activity style updates.

--- Simperium/src/main/java/com/simperium/LoginActivity.java
+++ Simperium/src/main/java/com/simperium/LoginActivity.java
@@ -16,6 +16,7 @@ import android.os.Bundle;
 import android.os.Looper;
 import android.view.View;
 import android.util.Log;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -58,6 +59,8 @@ public class LoginActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.setTheme(R.style.simperiumstyle);
 		setContentView(R.layout.login);
 
 		try {
