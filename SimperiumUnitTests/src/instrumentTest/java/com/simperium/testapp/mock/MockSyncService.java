2ff377256ed092b8a670e8b19ec338fc9d4df77b
Beau Collins <beaucollins@gmail.com>
2013-08-28 13:11:21 -0700

    MockSyncService now properly implements interface

--- SimperiumUnitTests/src/instrumentTest/java/com/simperium/testapp/mock/MockSyncService.java
+++ SimperiumUnitTests/src/instrumentTest/java/com/simperium/testapp/mock/MockSyncService.java
@@ -2,18 +2,18 @@ package com.simperium.testapp.mock;
 
 import com.simperium.client.SyncService;
 
-public class MockSyncService {
+public class MockSyncService implements SyncService {
 
     /**
      * Just runs the on the same thread.
      */
+    @Override
+    public void submit(Runnable runnable){
+        runnable.run();
+    }
+
     public static SyncService service(){
-        return new SyncService(){
-            @Override
-            public void submit(Runnable runnable){
-                runnable.run();
-            }
-        };
+        return new MockSyncService();
     }
 
 }
