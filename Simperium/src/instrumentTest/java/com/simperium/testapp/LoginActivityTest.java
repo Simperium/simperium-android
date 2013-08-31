c80d54c06ea770c2c0b73d041bbbfb3875f5c35b
Robert Collins <robert@automattic.com>
2013-08-30 22:35:21 -0700

    Tests for LoginActivity
    - makes sure the Sign Up vs Sign In forms display correctly
    - tests that the buttons correctly toggle the forms
    - tests EXTRA_EMAIL fills in provided email and focuses password field
    - tests EXTRA_SIGN_IN_FIRST displays the Sign In form by default
    - tests that Simperium provided user email address is prefilled and password field focused

--- Simperium/src/instrumentTest/java/com/simperium/testapp/LoginActivityTest.java
+++ Simperium/src/instrumentTest/java/com/simperium/testapp/LoginActivityTest.java
@@ -17,6 +17,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.test.TouchUtils;
+import android.content.Intent;
 
 public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity> {
 
@@ -24,48 +26,152 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
     protected MockClient mClient;
     protected Simperium mSimperium;
 
+    protected LoginActivity mActivity;
+    Button mSignupButton;
+    Button mSigninButton;
+    EditText mEmailTextField;
+    EditText mPasswordTextField;
+    EditText mPasswordTextField2;
+    TextView mHaveAccountTextView;
+    TextView mForgotPasswordButton;
+    TextView mHaveAccountButton;
+    TextView mCreateAccountButton;
+    TextView mTermsOfService;
+
     public LoginActivityTest() {
         super(LoginActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mClient = new MockClient();
+        mSimperium = Simperium.newClient("fake-app", "fake-secret", mClient);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        mSimperium.deauthorizeUser();
+        super.tearDown();
+    }
+
+    public LoginActivity setupActivity(boolean signInFirst){
+        if (signInFirst) {
+            Intent signInFirstIntent = new Intent();
+            signInFirstIntent.putExtra(LoginActivity.EXTRA_SIGN_IN_FIRST, true);
+            setActivityIntent(signInFirstIntent);
+        }
+        return setupActivity();
     }
 
     /**
      * Configures a MockClient for use in testing
      */
     public LoginActivity setupActivity(){
+
+        setActivityInitialTouchMode(false);
         LoginActivity activity = getActivity();
-        mClient = new MockClient();
-        mSimperium = new Simperium("fake-app", "fake-secret", mClient);
-        activity.setSimperium(mSimperium);
+  
+        // make sure all our fields are present
+        mSignupButton = (Button) activity.findViewById(R.id.signup_button);
+        mSigninButton = (Button) activity.findViewById(R.id.signin_button);
+        mEmailTextField = (EditText) activity.findViewById(R.id.email_address);
+        mPasswordTextField = (EditText) activity.findViewById(R.id.password);
+        mPasswordTextField2 = (EditText) activity.findViewById(R.id.password2);
+        mHaveAccountTextView = (TextView) activity.findViewById(R.id.no_account);
+        mForgotPasswordButton = (TextView) activity.findViewById(R.id.forgot_password_button);
+        mHaveAccountButton = (TextView) activity.findViewById(R.id.have_account_button);
+        mCreateAccountButton = (TextView) activity.findViewById(R.id.create_account_button);
+        mTermsOfService = (TextView) activity.findViewById(R.id.l_agree_terms_of_service);
+
+        mActivity = activity;
+
         return activity;
     }
 
-    @Override
-    protected void setUp() throws Exception {
-
-        super.setUp();
-
-        setActivityInitialTouchMode(false);
-    }
-
-    @UiThreadTest
-    public void testDefaultActivityLayout() throws Exception {
+    public void testDefaultSignUp() {
 
         LoginActivity activity = setupActivity();
 
-        assertNotNull(activity);
+        // by default it should display the sign up form controls
+        assertShowsSignUpForm();
+    }
 
-        // make sure all our fields are present
-        Button signupButton = (Button) activity.findViewById(R.id.signin_button);
-        Button signinButton = (Button) activity.findViewById(R.id.signin_button);
-        EditText emailTextField = (EditText) activity.findViewById(R.id.email_address);
-        EditText passwordTextField = (EditText) activity.findViewById(R.id.password);
-        EditText passwordTextField2 = (EditText) activity.findViewById(R.id.password2);
-        TextView haveAccountTextView = (TextView) activity.findViewById(R.id.no_account);
-        TextView forgotPasswordButton = (TextView) activity.findViewById(R.id.forgot_password_button);
-        TextView haveAccountButton = (TextView) activity.findViewById(R.id.have_account_button);
-        TextView createAccountButton = (TextView) activity.findViewById(R.id.create_account_button);
-        TextView termsOfService = (TextView) activity.findViewById(R.id.l_agree_terms_of_service);
+    public void testSignInFirst() {
 
+        // initialize the activity
+        setupActivity(true);
+
+        // it should display the sign in form controls
+        assertShowsSignInForm();
+    }
+
+    public void testToggleSignInForm(){
+        LoginActivity activity = setupActivity();
+
+        assertShowsSignUpForm();
+
+        TouchUtils.clickView(this, mHaveAccountButton);
+
+        assertShowsSignInForm();
+
+    }
+
+    public void testToggleSignUpForm(){
+        LoginActivity activity = setupActivity(true);
+
+        assertShowsSignInForm();
+
+        TouchUtils.clickView(this, mCreateAccountButton);
+
+        assertShowsSignUpForm();
+
+    }
+
+    public void testPrefillEmail(){
+        String email = "user@example.com";
+        Intent intent = new Intent();
+        intent.putExtra(LoginActivity.EMAIL_EXTRA, email);
+        setActivityIntent(intent);
+        setupActivity();
+
+        assertEquals(email, mEmailTextField.getText().toString());
+        assertEquals(mPasswordTextField, mActivity.getCurrentFocus());
+    }
+
+    public void testPrefillEmailFromUser(){
+        String email = "user@example.com";
+        mSimperium.getUser().setEmail(email);
+        setupActivity();
+
+        assertEquals(email, mEmailTextField.getText().toString());
+        assertEquals(mPasswordTextField, mActivity.getCurrentFocus());
+    }
+
+    protected void assertShowsSignInForm(){
+        assertEquals(View.GONE, mSignupButton.getVisibility());
+        assertEquals(View.VISIBLE, mSigninButton.getVisibility());
+        assertEquals(View.VISIBLE, mEmailTextField.getVisibility());
+        assertEquals(View.VISIBLE, mPasswordTextField.getVisibility());
+        assertEquals(View.GONE, mPasswordTextField2.getVisibility());
+        assertEquals(View.GONE, mTermsOfService.getVisibility());
+        assertEquals(View.GONE, mHaveAccountButton.getVisibility());
+        assertEquals(View.VISIBLE, mCreateAccountButton.getVisibility());
+        assertEquals(View.VISIBLE, mForgotPasswordButton.getVisibility());
+        assertEquals(mActivity.getString(R.string.no_account), mHaveAccountTextView.getText());
+    }
+
+    protected void assertShowsSignUpForm(){
+        assertEquals(View.VISIBLE, mSignupButton.getVisibility());
+        assertEquals(View.GONE, mSigninButton.getVisibility());
+        assertEquals(View.VISIBLE, mEmailTextField.getVisibility());
+        assertEquals(View.VISIBLE, mPasswordTextField.getVisibility());
+        assertEquals(View.VISIBLE, mPasswordTextField2.getVisibility());
+        assertEquals(View.VISIBLE, mTermsOfService.getVisibility());
+        assertEquals(View.VISIBLE, mHaveAccountButton.getVisibility());
+        assertEquals(View.GONE, mCreateAccountButton.getVisibility());
+        assertEquals(View.GONE, mForgotPasswordButton.getVisibility());
+        assertEquals(mActivity.getString(R.string.have_account), mHaveAccountTextView.getText());
     }
 
 }
