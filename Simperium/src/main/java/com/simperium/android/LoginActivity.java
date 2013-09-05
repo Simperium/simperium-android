ba350349703348c1f610948ff68d0b0315baec61
Robert Collins <robert@automattic.com>
2013-09-05 13:05:59 -0700

    Whitespace fix

--- Simperium/src/main/java/com/simperium/android/LoginActivity.java
+++ Simperium/src/main/java/com/simperium/android/LoginActivity.java
@@ -12,6 +12,7 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.net.ConnectivityManager;
 import android.net.Uri;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Looper;
 import android.view.View;
@@ -35,19 +36,19 @@ import android.util.Log;
 
 public class LoginActivity extends Activity {
 
-	public static final String TAG = "SimperiumLoginActivity";
+    public static final String TAG = "SimperiumLoginActivity";
 
     public static final String EXTRA_SIGN_IN_FIRST = "signInFirst";
     private static final String URL_FORGOT_PASSWORD = "https://simple-note.appspot.com/forgot/";
 
-	private ConnectivityManager mSystemService;
-	private ProgressDialog pd;
+    private ConnectivityManager mSystemService;
+    private ProgressDialog pd;
 
-	public static final String EMAIL_EXTRA = "email";
+    public static final String EMAIL_EXTRA = "email";
 
-	private EditText emailTextField;
-	private EditText passwordTextField;
-	private EditText passwordTextField2;
+    private EditText emailTextField;
+    private EditText passwordTextField;
+    private EditText passwordTextField2;
 
     private TextView haveAccountTextView;
     private TextView haveAccountButton;
@@ -58,43 +59,43 @@ public class LoginActivity extends Activity {
     private Button signupButton;
     private Button signinButton;
 
-	private Simperium mSimperium;
+    private Simperium mSimperium;
 
     public void setSimperium(Simperium simperium){
         mSimperium = simperium;
     }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setTheme(R.style.simperiumstyle);
-		setContentView(R.layout.login);
+        setContentView(R.layout.login);
 
-		try {
-			setSimperium(Simperium.getInstance());
-		} catch (SimperiumNotInitializedException e) {
-			Logger.log("Can't create the LoginActivity", e);
-		}
+        try {
+            setSimperium(Simperium.getInstance());
+        } catch (SimperiumNotInitializedException e) {
+            Logger.log("Can't create the LoginActivity", e);
+        }
 
-		mSystemService = (ConnectivityManager) getApplicationContext()
-				.getSystemService(Context.CONNECTIVITY_SERVICE);
+        mSystemService = (ConnectivityManager) getApplicationContext()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
 
-		signupButton = (Button) findViewById(R.id.signup_button);
-		signupButton.setOnClickListener(signupClickListener);
-		
-		signinButton = (Button) findViewById(R.id.signin_button);
-		signinButton.setOnClickListener(signinClickListener);
-		emailTextField = (EditText) findViewById(R.id.email_address);
+        signupButton = (Button) findViewById(R.id.signup_button);
+        signupButton.setOnClickListener(signupClickListener);
+        
+        signinButton = (Button) findViewById(R.id.signin_button);
+        signinButton.setOnClickListener(signinClickListener);
+        emailTextField = (EditText) findViewById(R.id.email_address);
 
-		passwordTextField = (EditText) findViewById(R.id.password);
-		passwordTextField2 = (EditText) findViewById(R.id.password2);
+        passwordTextField = (EditText) findViewById(R.id.password);
+        passwordTextField2 = (EditText) findViewById(R.id.password2);
 
         haveAccountTextView = (TextView) findViewById(R.id.no_account);
-		
-		Intent intent = getIntent();
-		if (intent.hasExtra(EMAIL_EXTRA)) {
-			emailTextField.setText(intent.getStringExtra(EMAIL_EXTRA));
+        
+        Intent intent = getIntent();
+        if (intent.hasExtra(EMAIL_EXTRA)) {
+            emailTextField.setText(intent.getStringExtra(EMAIL_EXTRA));
         } else if (mSimperium != null){
             emailTextField.setText(mSimperium.getUser().getEmail());
         }
@@ -116,7 +117,7 @@ public class LoginActivity extends Activity {
             }
         });
 
-		haveAccountButton = (TextView) findViewById(R.id.have_account_button);
+        haveAccountButton = (TextView) findViewById(R.id.have_account_button);
         haveAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -125,7 +126,7 @@ public class LoginActivity extends Activity {
             }
         });
 
-		createAccountButton = (TextView) findViewById(R.id.create_account_button);
+        createAccountButton = (TextView) findViewById(R.id.create_account_button);
         createAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -141,9 +142,9 @@ public class LoginActivity extends Activity {
             }
         });
 
-		termsOfService = (TextView) findViewById(R.id.l_agree_terms_of_service);
-		termsOfService.setClickable(true);
-		termsOfService.setOnClickListener(new View.OnClickListener() {
+        termsOfService = (TextView) findViewById(R.id.l_agree_terms_of_service);
+        termsOfService.setClickable(true);
+        termsOfService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Uri uriUrl = Uri.parse("https://simperium.com/tos/");
@@ -154,7 +155,7 @@ public class LoginActivity extends Activity {
 
         if (intent.hasExtra(EXTRA_SIGN_IN_FIRST))
             setSignInVisible();
-	}
+    }
 
     private void setSignInVisible() {
         haveAccountButton.setVisibility(View.GONE);
@@ -168,183 +169,184 @@ public class LoginActivity extends Activity {
     }
 
     private void registerUser(User user) {
-		setResult(RESULT_OK);
+        setResult(RESULT_OK);
         // Dismiss soft keyboard
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         if (inputMethodManager != null)
             inputMethodManager.hideSoftInputFromWindow(passwordTextField.getWindowToken(), 0);
-		finish();
-	}
+        finish();
+    }
 
-	private boolean checkUserData() {
-		// try to create the user
-		final String email = emailTextField.getText().toString().trim();
-		final String password = passwordTextField.getText().toString().trim();
+    private boolean checkUserData() {
+        // try to create the user
+        final String email = emailTextField.getText().toString().trim();
+        final String password = passwordTextField.getText().toString().trim();
 
-		if (email.equals("") || password.equals("")) {
-			AlertUtil.showAlert(LoginActivity.this, R.string.required_fields,
-					R.string.username_password_required);
-			return false;
-		}
-		
-		if (password.length() < 4 ) {
-			AlertUtil.showAlert(LoginActivity.this, R.string.invalid_password_title,
-					R.string.invalid_password_message);
-			return false;
-		}
+        if (email.equals("") || password.equals("")) {
+            AlertUtil.showAlert(LoginActivity.this, R.string.required_fields,
+                    R.string.username_password_required);
+            return false;
+        }
+        
+        if (password.length() < 4 ) {
+            AlertUtil.showAlert(LoginActivity.this, R.string.invalid_password_title,
+                    R.string.invalid_password_message);
+            return false;
+        }
 
-		final String emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
-		final Pattern emailRegExPattern = Pattern.compile(emailRegEx,
-				Pattern.DOTALL);
-		Matcher matcher = emailRegExPattern.matcher(email);
-		if (!matcher.find()) {
-			AlertUtil.showAlert(LoginActivity.this,
-					R.string.invalid_email_title,
-					R.string.invalid_email_message);
-			return false;
-		}
+        final String emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+        final Pattern emailRegExPattern = Pattern.compile(emailRegEx,
+                Pattern.DOTALL);
+        Matcher matcher = emailRegExPattern.matcher(email);
+        if (!matcher.find()) {
+            AlertUtil.showAlert(LoginActivity.this,
+                    R.string.invalid_email_title,
+                    R.string.invalid_email_message);
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	private void showLoginError(String message) {
-		if(pd != null ) 
-			pd.dismiss();
-		AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(
-				LoginActivity.this);
-		dialogBuilder.setTitle(getString(R.string.error));
-		dialogBuilder.setMessage(message);
-		dialogBuilder.setPositiveButton(getString(R.string.ok),
-				new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int whichButton) {
-						dialog.dismiss();
-					}
-				});
-		dialogBuilder.setCancelable(true);
-		dialogBuilder.create().show();
-	}
+    private void showLoginError(String message) {
+        if(pd != null ) 
+            pd.dismiss();
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(
+                LoginActivity.this);
+        dialogBuilder.setTitle(getString(R.string.error));
+        dialogBuilder.setMessage(message);
+        dialogBuilder.setPositiveButton(getString(R.string.ok),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dialog.dismiss();
+                    }
+                });
+        dialogBuilder.setCancelable(true);
+        dialogBuilder.create().show();
+    }
 
-	private void signUp() {
-		// try to create the user
-		final String email = emailTextField.getText().toString().trim();
-		final String password = passwordTextField.getText().toString().trim();
-		final String password2 = passwordTextField2.getText().toString().trim();
+    private void signUp() {
+        // try to create the user
+        final String email = emailTextField.getText().toString().trim();
+        final String password = passwordTextField.getText().toString().trim();
+        final String password2 = passwordTextField2.getText().toString().trim();
 
-		if (false == checkUserData())
-			return;
+        if (false == checkUserData())
+            return;
 
-		if( ! password2.equals(password) ) {
-			AlertUtil.showAlert(LoginActivity.this, R.string.invalid_password_title,
-					R.string.passwords_do_not_match_message);
-			return;
-		}
+        if( ! password2.equals(password) ) {
+            AlertUtil.showAlert(LoginActivity.this, R.string.invalid_password_title,
+                    R.string.passwords_do_not_match_message);
+            return;
+        }
 
-		pd = ProgressDialog.show(LoginActivity.this,
-				null,
-				getString(R.string.signing_up), true, false);
-		
-		mSimperium.createUser(email, password, new User.AuthResponseHandler() {
-			@Override
-			public void onSuccess(User user) {
-				if(pd != null ) 
-					pd.dismiss();
-				registerUser(user);
-			}
+        pd = ProgressDialog.show(LoginActivity.this,
+                null,
+                getString(R.string.signing_up), true, false);
+        
+        mSimperium.createUser(email, password, new User.AuthResponseHandler() {
+            @Override
+            public void onSuccess(User user) {
+                if(pd != null ) 
+                    pd.dismiss();
+                registerUser(user);
+            }
 
-			@Override
-			public void onInvalid(User user, Throwable error, JSONObject errors) {
-				showLoginError(errors.toString());
-			}
+            @Override
+            public void onInvalid(User user, Throwable error, JSONObject errors) {
+                showLoginError(errors.toString());
+            }
 
-			@Override
-			public void onFailure(User user, Throwable error, String response) {
-				showLoginError(response);
-			}
-		});
-	}
+            @Override
+            public void onFailure(User user, Throwable error, String response) {
+                showLoginError(response);
+            }
+        });
+    }
 
-	private void signIn() {
-		final String email = emailTextField.getText().toString().trim();
-		final String password = passwordTextField.getText().toString().trim();
+    private void signIn() {
+        final String email = emailTextField.getText().toString().trim();
+        final String password = passwordTextField.getText().toString().trim();
 
-		if (false == checkUserData())
-			return;
+        if (false == checkUserData())
+            return;
 
-		
-		pd = ProgressDialog.show(LoginActivity.this,
-				null,
-				getString(R.string.signing_in), true, false);
-		
-		mSimperium.authorizeUser(email, password,
-				new User.AuthResponseHandler() {
-					@Override
-					public void onSuccess(User user) {
-						if(pd != null) 
-							pd.dismiss();
-						registerUser(user);
-					}
+        
+        pd = ProgressDialog.show(LoginActivity.this,
+                null,
+                getString(R.string.signing_in), true, false);
+        
+        mSimperium.authorizeUser(email, password,
+                new User.AuthResponseHandler() {
+                    @Override
+                    public void onSuccess(User user) {
+                        if(pd != null) 
+                            pd.dismiss();
+                        registerUser(user);
+                    }
 
-					@Override
-					public void onInvalid(User user, Throwable error, JSONObject errors) {
-						if(error instanceof org.apache.http.client.HttpResponseException) {
-							org.apache.http.client.HttpResponseException errorObj = (org.apache.http.client.HttpResponseException) error;
-							if(errorObj.getStatusCode() == 401)
-								showLoginError(getString(R.string.login_failed_message));
-							else
-								showLoginError(error.toString());
-						}
-					}
+                    @Override
+                    public void onInvalid(User user, Throwable error, JSONObject errors) {
+                        if(error instanceof org.apache.http.client.HttpResponseException) {
+                            org.apache.http.client.HttpResponseException errorObj = (org.apache.http.client.HttpResponseException) error;
+                            if(errorObj.getStatusCode() == 401)
+                                showLoginError(getString(R.string.login_failed_message));
+                            else
+                                showLoginError(error.toString());
+                        }
+                    }
 
-					@Override
-					public void onFailure(User user, Throwable error, String response) {
-						if(error instanceof org.apache.http.client.HttpResponseException) {
-							org.apache.http.client.HttpResponseException errorObj = (org.apache.http.client.HttpResponseException) error;
-							if(errorObj.getStatusCode() == 401)
-								showLoginError(getString(R.string.login_failed_message));
-							else
-								showLoginError(response);
-						}
-					}
-				});
-	}
+                    @Override
+                    public void onFailure(User user, Throwable error, String response) {
+                        if(error instanceof org.apache.http.client.HttpResponseException) {
+                            org.apache.http.client.HttpResponseException errorObj = (org.apache.http.client.HttpResponseException) error;
+                            if(errorObj.getStatusCode() == 401)
+                                showLoginError(getString(R.string.login_failed_message));
+                            else
+                                showLoginError(response);
+                        }
+                    }
+                });
+    }
 
-	View.OnClickListener signupClickListener = new View.OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			startSignUpOrSignin(true);
-		}
-	};
+    View.OnClickListener signupClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startSignUpOrSignin(true);
+        }
+    };
 
-	View.OnClickListener signinClickListener = new View.OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			startSignUpOrSignin(false);
-		}
-	};
+    View.OnClickListener signinClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startSignUpOrSignin(false);
+        }
+    };
 
-	private void startSignUpOrSignin(final boolean isSignup) {
-		if (mSystemService.getActiveNetworkInfo() == null) {
-			AlertUtil.showAlert(LoginActivity.this, R.string.no_network_title,
-					R.string.no_network_message);
-		} else {
-			Thread action = new Thread() {
-				public void run() {
-					Looper.prepare();
-					if (isSignup == true)
-						signUp();
-					else
-						signIn();
-					Looper.loop();
-				}
-			};
-			action.start();
-		}
-	}
-	
-	@Override
-	public void onBackPressed() {
-		setResult(RESULT_CANCELED);
-		finish();
-	}
-	
+    private void startSignUpOrSignin(final boolean isSignup) {
+        
+        if (mSystemService.getActiveNetworkInfo() == null) {
+            AlertUtil.showAlert(LoginActivity.this, R.string.no_network_title,
+                    R.string.no_network_message);
+        } else {
+            Thread action = new Thread() {
+                public void run() {
+                    Looper.prepare();
+                    if (isSignup == true)
+                        signUp();
+                    else
+                        signIn();
+                    Looper.loop();
+                }
+            };
+            action.start();
+        }
+    }
+    
+    @Override
+    public void onBackPressed() {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+    
 }
