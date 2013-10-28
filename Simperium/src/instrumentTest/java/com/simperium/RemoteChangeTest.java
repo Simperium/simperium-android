79113c4305f7fef92a9cebb27b29947167298f01
Robert Collins <robert@automattic.com>
2013-10-27 21:26:25 -0700

    Unit tests for RemoteChange.buildFromMap
    - Finding bugs where remote add operations are not being applied

---
+++ Simperium/src/instrumentTest/java/com/simperium/RemoteChangeTest.java
@@ -0,0 +1,44 @@
+package com.simperium;
+
+import com.simperium.client.RemoteChange;
+
+import org.json.JSONObject;
+
+import junit.framework.TestCase;
+
+public class RemoteChangeTest extends TestCase {
+
+    public void testParseRemoteAddOperation() throws Exception {
+        String changeString = "{\"ccids\":[\"abc\"],\"ev\":1, \"id\":\"mock\",\"clientid\":\"mock-client\",\"o\":\"M\",\"v\":{}}";
+        JSONObject changeJSON = new JSONObject(changeString);
+
+        RemoteChange change = RemoteChange.buildFromMap(changeJSON);
+
+        assertTrue(change.isAddOperation());
+        assertFalse(change.isModifyOperation());
+        assertFalse(change.isRemoveOperation());
+
+    }
+
+    public void testParseRemoteDeleteOperation() throws Exception {
+        String changeString = "{\"ccids\":[\"abc\"],\"ev\":2, \"id\":\"mock\",\"clientid\":\"mock-client\",\"o\":\"-\"}";
+        JSONObject changeJSON = new JSONObject(changeString);
+
+        RemoteChange change = RemoteChange.buildFromMap(changeJSON);
+        assertTrue(change.isRemoveOperation());
+        assertFalse(change.isAddOperation());
+        assertFalse(change.isModifyOperation());
+    }
+
+    public void testParseRemoteModifyOperation() throws Exception {
+        String changeString = "{\"ccids\":[\"abc\"],\"ev\":2,\"sv\":1,\"id\":\"mock\",\"clientid\":\"mock-client\",\"o\":\"M\",\"v\":{}}";
+        JSONObject changeJSON = new JSONObject(changeString);
+
+        RemoteChange change = RemoteChange.buildFromMap(changeJSON);
+
+        assertTrue(change.isModifyOperation());
+        assertFalse(change.isRemoveOperation());
+        assertFalse(change.isAddOperation());
+    }
+
+}
