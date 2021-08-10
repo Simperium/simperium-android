67d14493f1b3f595e57f486858acd012f4669867
danilodominguezperez <danilo.dominguez@automattic.com>
2021-08-10 14:56:57 -0500

    Handle two kind of results for 401 responses during authentication
    For authentication, we can get responses with 401 for invalid
    credentials or compromised password. We recognize such responses by the
    value of theri bodies which is obtained through the Throwable's message.

--- Simperium/src/main/java/com/simperium/client/AuthException.java
+++ Simperium/src/main/java/com/simperium/client/AuthException.java
@@ -2,11 +2,14 @@ package com.simperium.client;
 
 import com.simperium.SimperiumException;
 
+import java.util.Objects;
+
 public class AuthException extends SimperiumException {
 
     static public final String GENERIC_FAILURE_MESSAGE = "Invalid username or password";
     static public final String EXISTING_USER_FAILURE_MESSAGE = "Account already exists";
     static public final String COMPROMISED_PASSWORD_MESSAGE = "Password has been compromised";
+    static public final String INVALID_LOGIN_BODY = "invalid login";
 
     static public final int ERROR_STATUS_CODE = -1;
 
@@ -39,7 +42,13 @@ public class AuthException extends SimperiumException {
             case 409:
                 return new AuthException(FailureType.EXISTING_ACCOUNT, EXISTING_USER_FAILURE_MESSAGE, cause);
             case 401:
-                return new AuthException(FailureType.COMPROMISED_PASSWORD, COMPROMISED_PASSWORD_MESSAGE, cause);
+                // Code 401 can be obtain because credentials are wrong or the user's password has been compromised
+                // To differentiate both responses, we check the response's body
+                if (cause != null && Objects.equals(cause.getMessage(), INVALID_LOGIN_BODY)) {
+                    return new AuthException(FailureType.INVALID_ACCOUNT, GENERIC_FAILURE_MESSAGE, cause);
+                } else {
+                    return new AuthException(FailureType.COMPROMISED_PASSWORD, COMPROMISED_PASSWORD_MESSAGE, cause);
+                }
             default:
                 return new AuthException(FailureType.INVALID_ACCOUNT, GENERIC_FAILURE_MESSAGE, cause);
         }
