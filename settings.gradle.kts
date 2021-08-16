9e496aa7f9d16d239577400ddaa5ce5d7d72c787
Oguz Kocer <oguz.kocer@automattic.com>
2021-08-16 14:13:32 -0400

    Adds publish-to-s3 plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,16 @@
 pluginManagement {
     plugins {
         id("com.android.library") version "4.2.2"
+        id("com.automattic.android.publish-to-s3") version "0.6.1"
     }
     repositories {
+        maven {
+            url = uri("https://a8c-libs.s3.amazonaws.com/android")
+            content {
+                includeGroup("com.automattic.android")
+                includeGroup("com.automattic.android.publish-to-s3")
+            }
+        }
         gradlePluginPortal()
         google()
     }
