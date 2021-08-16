ed86eea11e8b3faea89699e2c8c5460352994097
Oguz Kocer <oguz.kocer@automattic.com>
2021-08-16 14:00:52 -0400

    Use plugin DSL for Android Gradle Plugin

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    plugins {
+        id("com.android.library") version "4.2.2"
+    }
+    repositories {
+        gradlePluginPortal()
+        google()
+    }
+}
+
+include(":Simperium")
